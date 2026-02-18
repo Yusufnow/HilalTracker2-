9ad5789b21bafd204381dbb1ece90fb8e69b128a
Yusufnow <58217675+Yusufnow@users.noreply.github.com>
2026-02-18 05:21:26 +0100

    Update sky3d.js

--- sky3d.js
+++ sky3d.js
@@ -1,1 +1,27 @@
-console.log("3D Himmel geladen");
+let scene, camera, renderer, moonMesh;
+
+function init3DSky(){
+
+    const canvas = document.getElementById("sky3d");
+
+    scene = new THREE.Scene();
+
+    camera = new THREE.PerspectiveCamera(75, canvas.clientWidth/canvas.clientHeight,0.1,1000);
+    renderer = new THREE.WebGLRenderer({canvas,alpha:true});
+
+    const geometry = new THREE.SphereGeometry(2,32,32);
+    const material = new THREE.MeshBasicMaterial({color:0xffffff});
+    moonMesh = new THREE.Mesh(geometry,material);
+
+    scene.add(moonMesh);
+
+    camera.position.z=5;
+
+    animate();
+}
+
+function animate(){
+    requestAnimationFrame(animate);
+    moonMesh.rotation.y+=0.002;
+    renderer.render(scene,camera);
+}
